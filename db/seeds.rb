8c69680d62b5c376421e2e608ea496944139708a
filipe custodio <filipe.l.custodio@gmail.com>
2018-11-29 17:34:46 +0000

    Logic all done. Just playing with css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cocktail.create(name: "Mojito",)
+Cocktail.create(name: "Caipirinha")
+Cocktail.create(name: "Cosmopolitan")
+Cocktail.create(name: "Whisky on the rocks")
+
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
